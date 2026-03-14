fdc069293a7d16ea8962f0bbbf62b153db0ea8bc
Rafael-Tunes-Mathiensen <rafaelmathiensen@gmail.com>
2026-03-13 22:08:10 -0300

    feat: aprimorar sistema de cameras e deixar individual por fase

---
+++ plataforma-2d/scripts/camera.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+var target: Node2D
+
+func _ready() -> void:
+	get_target()
+
+func _process(delta: float) -> void:
+	if target:
+		position = target.position
+
+func get_target():
+	var nodes = get_tree().get_nodes_in_group("player")
+	if nodes.size() == 0:
+		push_error("Nenhum Player Encontrado!")
+		return
+	
+	target = nodes[0]
